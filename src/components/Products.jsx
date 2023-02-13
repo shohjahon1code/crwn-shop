ed8d8f35c5dc45b1f1975db2e3f042c6a72b7e0e
shohjahon1code <suyunovshohjahon08@gmail.com>
2023-02-13 17:47:38 +0500

    add-pagination

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,21 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { LoaderContext } from "../context/loader-context";
 import Loader from "./Loader";
+import Pagination from "./Pagination";
 import Product from "./Product";
+
+let PageSize = 6;
 
 const Products = () => {
   const [products, setProducts] = useState([]);
   const { loading, setLoading } = useContext(LoaderContext);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return products.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, products]);
 
   useEffect(() => {
     try {
@@ -31,11 +41,22 @@ const Products = () => {
     return <Loader />;
   }
   return (
-    <div  className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 container mt-20">
-      {products.map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 container mt-20 mb-3">
+        {currentTableData.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+      <div className="mt-7 mx-auto">
+        <Pagination
+          className="pagination-bar"
+          currentPage={currentPage}
+          totalCount={products.length}
+          pageSize={PageSize}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      </div>
+    </>
   );
 };
 
